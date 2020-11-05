d29acdd8efcd7e64ea7a59dcdae1ee8b24857b68
chrishargan <chrishargan@gmail.com>
2020-11-05 17:08:03 +0100

    Setting up dynamic state change for burger order

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Burger.css';
 import BurgerIngredient from './BurgerIngedients/BurgerIngredients';
 
+
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + 1} type={igKey} />;
+            });
+        });
+    console.log(transformedIngredients);
     return (
         <div className="Burger" >
             <BurgerIngredient type="Bread-Top" />
-            <BurgerIngredient type="Cheese" />
-            <BurgerIngredient type="Meat" />
+            {transformedIngredients}
             <BurgerIngredient type="Bread-Bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    /* constructor(props) {
+         super(props);
+         this.state={...}
+     }
+     */
+    state = {
+        ingredients: {
+            Salad: 0,
+            Onion: 2,
+            Pickles: 1,
+            Bacon: 0,
+            Cheese: 0,
+            Meat: 2
+        }
+    }
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
